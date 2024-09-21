7c008dcaa8b07c96ed9774302645ae696e4eba37
anryzhakov <anryzhakov@yandex.ru>
2024-09-21 17:23:41 +0300

    v.1.2

---
+++ Module_2/hurd.py
@@ -0,0 +1,26 @@
+# Слушатель курса Junior Python-разработчик
+# Рыжаков Андрей Николаевич, anryzhakov@yandex.ru
+# Дополнительное практическое задание по модулю:
+# "Основные операторы"
+
+import random
+
+firstFieldList = []
+passFieldList = []
+Item = 3
+while Item <= 20:
+    firstFieldList.append(Item)
+    Item +=1
+firstField = random.choice(firstFieldList)
+
+print('Число первой вставки - ', firstField)
+
+for i in range(firstField): #firstField)
+    for j in range(firstField):
+        k = (i + 1) + (j + 1)
+        if firstField > 3:
+            if (firstField % k) == 0 and (i < j)  :
+                passFieldList.append(i +1)
+                passFieldList.append(j +1)
+        else: continue
+print('Пароль - ', passFieldList)
